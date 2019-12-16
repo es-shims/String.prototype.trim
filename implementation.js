b0378c99988846b0a8edf3fcfd08f30dd632f5a9
Jordan Harband <ljharb@gmail.com>
2019-12-15 23:53:58 -0800

    [Refactor] use split-up `es-abstract` (57% bundle size decrease)

--- implementation.js
+++ implementation.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var bind = require('function-bind');
-var ES = require('es-abstract/es5');
-var replace = bind.call(Function.call, String.prototype.replace);
+var CheckObjectCoercible = require('es-abstract/5/CheckObjectCoercible');
+var ToString = require('es-abstract/2019/ToString');
+var callBound = require('es-abstract/helpers/callBound');
+var $replace = callBound('String.prototype.replace');
 
 /* eslint-disable no-control-regex */
 var leftWhitespace = /^[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+/;
@@ -10,6 +11,6 @@ var rightWhitespace = /[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u20
 /* eslint-enable no-control-regex */
 
 module.exports = function trim() {
-	var S = ES.ToString(ES.CheckObjectCoercible(this));
-	return replace(replace(S, leftWhitespace, ''), rightWhitespace, '');
+	var S = ToString(CheckObjectCoercible(this));
+	return $replace($replace(S, leftWhitespace, ''), rightWhitespace, '');
 };
